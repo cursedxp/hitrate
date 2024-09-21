85f1677251c98b54a9751d2fd61e906c4b134766
Anil Ozsoy <anilozsoy@gmail.com>
2024-09-21 23:40:12 +0300

    ✨ (layout.js): add ThemeWrapper component and Provider from react-redux to wrap the children components in the RootLayout component 🔧 (layout.js): import store from redux/store to provide the store to the Provider component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,14 @@
+"use client";
 import "./globals.css";
-
+import ThemeWrapper from "./components/themeWrapper/themeWrapper";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <Provider store={store}>
+        <ThemeWrapper>{children}</ThemeWrapper>
+      </Provider>
     </html>
   );
 }
