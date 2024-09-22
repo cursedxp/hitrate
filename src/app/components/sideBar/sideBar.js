391093aa1354a82a851271ba84a3962ab3725de3
Anil Ozsoy <anilozsoy@gmail.com>
2024-09-22 18:03:31 +0300

    ✨ (sideBar.js): add width style to the side bar to ensure consistent sizing ♻️ (sideBar.js): refactor the layout of the side bar to improve readability and organization ✨ (sideBar.js): add a "Thumbnails" section to the side bar for better categorization of content

--- src/app/components/sideBar/sideBar.js
+++ src/app/components/sideBar/sideBar.js
@@ -1,23 +1,24 @@
 "use client";
-
 import Button from "../button/button";
-import { Sidebar, Menu } from "react-feather";
 import SideBarContent from "./sideBarContent";
-import { useState } from "react";
 import ThemeToggle from "../themeToggle/themeToggle";
 import ShuffleButton from "../shuffleButton/shuffleButton";
+import { Menu } from "react-feather";
 
 export default function SideBar() {
   return (
-    <div className="absolute top-5 left-5 z-10 bg-white shadow-md dark:bg-zinc-900 rounded-xl">
-      <div className="flex gap-2 p-4 items-center">
+    <div className="absolute top-5 left-5 z-10 bg-white shadow-md dark:bg-zinc-900 rounded-xl w-[300px]">
+      <div className="flex gap-2 px-2 py-2 items-center border-b border-zinc-200 dark:border-zinc-800">
         <Button className="bg-white">
           <Menu className="w-4 h-4 dark:text-white" />
         </Button>
         <span className="text-sm font-bold">ProjectName</span>
-        <ThemeToggle />
-        <ShuffleButton />
+        <div className="flex gap-2 justify-end flex-1">
+          <ThemeToggle />
+          <ShuffleButton />
+        </div>
       </div>
+      <div className="text-sm font-bold p-2">Thumbnails</div>
       <SideBarContent />
     </div>
   );
