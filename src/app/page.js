bce625afc38f7fc8ae43150bc2f106f79a3fc655
Anil Ozsoy <anilozsoy@gmail.com>
2024-09-22 16:56:52 +0300

    ♻️ (page.js): remove unused imports ThemeToggle and ShuffleButton to improve code cleanliness and reduce unnecessary dependencies

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,3 @@
-import ThemeToggle from "./components/themeToggle/themeToggle";
-import ShuffleButton from "./components/shuffleButton/shuffleButton";
 import SideBar from "./components/sideBar/sideBar";
 export default function Home() {
   return (
