046fd2ab73b78bdba8fd497bd9aea503e1f803d0
Anil Ozsoy <anilozsoy@gmail.com>
2024-09-22 16:53:21 +0300

    ♻️ (layout.js): refactor import statements and component names for improved clarity and consistency ✨ (layout.js): replace Provider component with StoreProvider component for Redux store integration ✨ (layout.js): replace ThemeWrapper component with ThemeProvider component for theme integration 📝 (layout.js): update HTML structure to wrap children components with StoreProvider and ThemeProvider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,12 @@
 "use client";
 import "./globals.css";
-import ThemeWrapper from "./components/themeWrapper/themeWrapper";
-import { Provider } from "react-redux";
-import store from "./redux/store";
+import ThemeProvider from "./components/themeProvider/themeProvider";
+import StoreProvider from "./redux/storeProvider";
+import { useSelector } from "react-redux";
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <Provider store={store}>
-        <ThemeWrapper>{children}</ThemeWrapper>
-      </Provider>
-    </html>
+    <StoreProvider>
+      <ThemeProvider>{children}</ThemeProvider>
+    </StoreProvider>
   );
 }
