a922b40092ff07b1f55e1d2b9a619fb6027a42e4
Anil Ozsoy <anilozsoy@gmail.com>
2024-09-22 18:03:09 +0300

    ♻️ (button.js): refactor button component class names to improve readability and consistency

--- src/app/components/button/button.js
+++ src/app/components/button/button.js
@@ -1,7 +1,7 @@
 export default function Button({ children, onClick, className }) {
   return (
     <button
-      className={`p-2 rounded-md bg-zinc-200 dark:bg-zinc-800 hover:bg-zinc-200 hover:dark:bg-zinc-600 ${className}`}
+      className={`p-2 rounded-md dark:bg-zinc-800 hover:bg-zinc-100 hover:dark:bg-zinc-600 ${className}`}
       onClick={onClick}
     >
       {children}
