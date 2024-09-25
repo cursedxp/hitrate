bdc16aa3ad4820c79608f847ab0b171b6b4b213d
Anil Ozsoy <anilozsoy@gmail.com>
2024-09-25 22:02:17 +0200

    ✨ (sideBarContent.js): add support for displaying thumbnail previews and uploading files 📝 (sideBarContent.js): update component to use Redux to access thumbnail previews from the state ♻️ (sideBarContent.js): refactor code to use separate components for image preview and file uploader 🔧 (sideBarContent.js): import necessary dependencies and configure client-side usage

--- src/app/components/sideBar/sideBarContent.js
+++ src/app/components/sideBar/sideBarContent.js
@@ -1,21 +1,22 @@
-import Image from "next/image";
-import { Upload } from "react-feather";
+"use client";
+import ImagePreview from "./fileUploader/imagePreview";
+import FileUploader from "./fileUploader/fileUploader";
+import { useSelector } from "react-redux";
 export default function SideBarContent() {
+  const thumbnailPreviews = useSelector(
+    (state) => state.thumbnail.thumbnailPreviews
+  );
+
   return (
     <div className="flex flex-col p-2">
+      <div className="flex justify-between text-sm font-bold pb-2">
+        Thumbnails
+      </div>
       <div className="grid grid-cols-2 gap-2">
-        <div className="relative h-[90px] w-[138px] rounded-md cursor-pointer">
-          <Image
-            src="https://placehold.co/600x400/png"
-            alt="image 1"
-            fill
-            sizes="100%"
-            className=" object-cover rounded-md"
-          />
-        </div>
-        <div className="border-2 border-blue-500 flex items-center justify-center h-[90px] w-[138px] rounded-md">
-          <Upload className="w-4 h-4 text-blue-500" />
-        </div>
+        {thumbnailPreviews.map((preview, index) => (
+          <ImagePreview key={index} image={preview} />
+        ))}
+        <FileUploader />
       </div>
     </div>
   );
