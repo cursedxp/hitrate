c566a472b6fee4ef4b5e9db87a181e230cab341f
Anil Ozsoy <anilozsoy@gmail.com>
2024-09-25 23:13:40 +0200

    ✨ (sideBarContent.js): add isLoading prop to ImagePreview component to display loading state for the last thumbnail preview

--- src/app/components/sideBar/sideBarContent.js
+++ src/app/components/sideBar/sideBarContent.js
@@ -2,10 +2,12 @@
 import ImagePreview from "./fileUploader/imagePreview";
 import FileUploader from "./fileUploader/fileUploader";
 import { useSelector } from "react-redux";
+
 export default function SideBarContent() {
   const thumbnailPreviews = useSelector(
     (state) => state.thumbnail.thumbnailPreviews
   );
+  const isLoading = useSelector((state) => state.thumbnail.isLoading);
 
   return (
     <div className="flex flex-col p-2">
@@ -14,7 +16,11 @@ export default function SideBarContent() {
       </div>
       <div className="grid grid-cols-2 gap-2">
         {thumbnailPreviews.map((preview, index) => (
-          <ImagePreview key={index} image={preview} />
+          <ImagePreview
+            key={index}
+            thumbnail={preview}
+            isLoading={isLoading && index === thumbnailPreviews.length - 1}
+          />
         ))}
         <FileUploader />
       </div>
