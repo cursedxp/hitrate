7af2ad735e7392fb8541fdfff82d433bdd233740
Anil Ozsoy <anilozsoy@gmail.com>
2024-09-25 22:03:01 +0200

    ✨ (store.js): add thumbnailReducer to the store configuration to handle thumbnail-related state management

--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/user.slice";
 import appReducer from "./slices/app.slice";
-
+import thumbnailReducer from "./slices/thumbnail.slice";
 const store = configureStore({
   reducer: {
     user: userReducer,
     app: appReducer,
+    thumbnail: thumbnailReducer,
   },
 });
 
