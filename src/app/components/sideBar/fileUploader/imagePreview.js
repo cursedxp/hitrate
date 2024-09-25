208e4289f3b958786a9f3cab36e62d6185964466
Anil Ozsoy <anilozsoy@gmail.com>
2024-09-25 23:14:23 +0200

    ✨ (imagePreview.js): add loader component to show loading state while image is being loaded 🐛 (imagePreview.js): fix imageLoaded state not being set correctly when thumbnail prop changes

--- src/app/components/sideBar/fileUploader/imagePreview.js
+++ src/app/components/sideBar/fileUploader/imagePreview.js
@@ -1,15 +1,30 @@
 import Image from "next/image";
+import Loader from "./loader";
+import { useState, useEffect } from "react";
 
-export default function ImagePreview({ image }) {
+export default function ImagePreview({ thumbnail, isLoading }) {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  useEffect(() => {
+    if (thumbnail) {
+      const img = new window.Image();
+      img.onload = () => setImageLoaded(true);
+      img.src = thumbnail;
+    }
+  }, [thumbnail]);
+
   return (
     <div className="relative h-[90px] w-[138px] rounded-md cursor-pointer">
-      <Image
-        src={image}
-        alt="image"
-        fill
-        sizes="100%"
-        className=" object-cover rounded-md"
-      />
+      {(isLoading || !imageLoaded) && <Loader />}
+      {imageLoaded && (
+        <Image
+          src={thumbnail}
+          alt="image"
+          fill
+          sizes="100%"
+          className="object-cover rounded-md"
+        />
+      )}
     </div>
   );
 }
