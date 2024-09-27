1a8921e8f7c693c54f8d2ffa4f0de4d005c3ef3b
Anil Ozsoy <anilozsoy@gmail.com>
2024-09-27 13:57:53 +0200

    ✨ (thumbnails.js): add a message to display when there are no thumbnail previews 📝 (thumbnails.js): add an index prop to the ImagePreview component to keep track of the index of each thumbnail

--- src/app/components/sideBar/thumbnails/thumbnails.js
+++ src/app/components/sideBar/thumbnails/thumbnails.js
@@ -12,9 +12,15 @@ export default function Thumbnails() {
   return (
     <>
       <div className="flex flex-col gap-4 p-2">
+        {thumbnailPreviews.length === 0 && (
+          <div className=" bg-blue-100 text-blue-500 px-2 py-6 text-center rounded-md text-sm">
+            Start adding your thumbnails here
+          </div>
+        )}
         {thumbnailPreviews.map((preview, index) => (
           <ImagePreview
             key={index}
+            index={index}
             thumbnail={preview}
             isLoading={isLoading && index === thumbnailPreviews.length - 1}
           />
