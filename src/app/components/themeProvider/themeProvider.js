3087e5081526861cdf0f34cc21ce5e73fb31311b
Anil Ozsoy <anilozsoy@gmail.com>
2024-09-27 11:52:23 +0200

    📝 (themeProvider.js): update body class name to use a more descriptive class name for dark mode background color

--- src/app/components/themeProvider/themeProvider.js
+++ src/app/components/themeProvider/themeProvider.js
@@ -6,7 +6,7 @@ export default function ThemeProvider({ children }) {
   const theme = useSelector((state) => state.app.theme);
   return (
     <html lang="en" className={theme}>
-      <body className="bg-white dark:bg-black dark:text-white h-screen">
+      <body className="bg-white dark:bg-zinc-900 dark:text-white h-screen">
         <Toaster position="top-right" />
         {/* <Header /> */}
         <main>{children}</main>
