f5e452d517d98f97725b4a3f93f0529f71a2c2d2
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-04 15:36:29 +0200

    ♻️ (themeProvider.js): remove commented out code for Header component to improve code readability

--- src/app/components/themeProvider/themeProvider.js
+++ src/app/components/themeProvider/themeProvider.js
@@ -1,14 +1,12 @@
 "use client";
 import { Toaster } from "react-hot-toast";
 import { useSelector } from "react-redux";
-import Header from "../header/header";
 export default function ThemeProvider({ children }) {
   const theme = useSelector((state) => state.app.theme);
   return (
     <html lang="en" className={theme}>
       <body className="bg-white dark:bg-zinc-900 dark:text-white h-screen">
         <Toaster position="top-right" />
-        {/* <Header /> */}
         <main>{children}</main>
       </body>
     </html>
