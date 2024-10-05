187a67365f6e257e6d996c89fbdaafe8d8c7c2de
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-05 19:51:40 +0200

    ♻️ (imagePreview.js): refactor CSS class names for better readability and consistency 📝 (imagePreview.js): remove unnecessary div element for horizontal line separator

--- src/app/components/fileUploader/imagePreview.js
+++ src/app/components/fileUploader/imagePreview.js
@@ -57,11 +57,10 @@ export default function ImagePreview({ thumbnail, isLoading, index }) {
 
       <div className="absolute top-0 left-0 w-full h-full bg-black/20 opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-md ">
         <div className="flex justify-end">
-          <div className="flex flex-col bg-zinc-200 dark:bg-zinc-800 rounded-md m-2">
+          <div className="flex flex-col bg-white dark:bg-zinc-800 rounded-md m-2">
             <button className="p-2 rounded-md hover:bg-zinc-700 hover:text-white text-black dark:text-white">
               <Eye size={16} />
             </button>
-            <div className="h-[1px] w-full bg-zinc-300 dark:bg-zinc-700"></div>
             <button
               className="p-2 rounded-md hover:bg-zinc-700 hover:text-white text-black dark:text-white"
               onClick={handleDelete}
