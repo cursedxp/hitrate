055a5bb6ce330f4f991966c5ecbe392d0ed84f01
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-05 22:24:36 +0200

    ✨ (video.js): create a new component `Video` to display individual video details ♻️ (videos.js): refactor the `Videos` component to use the new `Video` component for rendering each video

---
+++ src/app/components/videos/video.js
@@ -0,0 +1,41 @@
+import { Formatter } from "../utils/formatters";
+
+export default function Video({ video }) {
+  const formatter = Formatter();
+  return (
+    <div key={video.id} className="flex flex-col dark:text-white ">
+      <div className="relative pb-[56.25%]">
+        <img
+          src={video.snippet.thumbnails.medium.url}
+          alt={video.snippet.title}
+          className="absolute top-0 left-0 w-full h-full object-cover rounded-xl"
+        />
+        <div className="absolute bottom-1 right-1 bg-black bg-opacity-70 text-white text-xs px-1 py-0.5 rounded">
+          {video.snippet.duration}
+        </div>
+      </div>
+      <div className="flex gap-4 mt-2">
+        <img
+          src={video.snippet.thumbnails.default.url}
+          alt={video.snippet.channelTitle}
+          className="rounded-full w-9 h-9"
+        />
+        <div className="flex flex-col">
+          <div className="text-sm font-semibold line-clamp-2 mb-1">
+            {video.snippet.title}
+          </div>
+          <p className="text-xs text-gray-400">{video.snippet.channelTitle}</p>
+          <div className="flex items-center text-sm text-gray-400 mt-1">
+            <span>
+              {formatter.formatViewCount(video.statistics.viewCount)} views
+            </span>
+            <span className="mx-1">•</span>
+            <span>
+              {formatter.formatPublishedAt(video.snippet.publishedAt)}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/components/videos/videos.js
+++ src/app/components/videos/videos.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Image } from "next/image";
-import { Formatter } from "../utils/formatters";
-
+import Video from "./video";
 export default function Videos({
   initialRegionCode,
   initialCategory,
@@ -47,49 +45,11 @@ export default function Videos({
 
   if (error) return <div>Error: {error}</div>;
 
-  const formatter = Formatter();
-
   return (
     <div className="flex w-full items-center justify-center p-4">
       <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 w-full">
         {videos.map((video) => (
-          <div key={video.id} className="flex flex-col dark:text-white ">
-            <div className="relative pb-[56.25%]">
-              <img
-                src={video.snippet.thumbnails.medium.url}
-                alt={video.snippet.title}
-                className="absolute top-0 left-0 w-full h-full object-cover rounded-xl"
-              />
-              <div className="absolute bottom-1 right-1 bg-black bg-opacity-70 text-white text-xs px-1 py-0.5 rounded">
-                {video.snippet.duration}
-              </div>
-            </div>
-            <div className="flex gap-4 mt-2">
-              <img
-                src={video.snippet.thumbnails.default.url}
-                alt={video.snippet.channelTitle}
-                className="rounded-full w-9 h-9"
-              />
-              <div className="flex flex-col">
-                <div className="text-sm font-semibold line-clamp-2 mb-1">
-                  {video.snippet.title}
-                </div>
-                <p className="text-xs text-gray-400">
-                  {video.snippet.channelTitle}
-                </p>
-                <div className="flex items-center text-sm text-gray-400 mt-1">
-                  <span>
-                    {formatter.formatViewCount(video.statistics.viewCount)}{" "}
-                    views
-                  </span>
-                  <span className="mx-1">•</span>
-                  <span>
-                    {formatter.formatPublishedAt(video.snippet.publishedAt)}
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Video key={video.id} video={video} />
         ))}
       </div>
       {loading && (
