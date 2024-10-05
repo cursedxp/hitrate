ca9b94e210a136f3db65ab9744ad8268ba71e14f
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-05 22:03:08 +0200

    🎨 (themeProvider.js): update body background color and text color classes to match the new theme styling

--- src/app/components/themeProvider/themeProvider.js
+++ src/app/components/themeProvider/themeProvider.js
@@ -5,7 +5,7 @@ export default function ThemeProvider({ children }) {
   const theme = useSelector((state) => state.app.theme);
   return (
     <html lang="en" className={theme}>
-      <body className="bg-white dark:bg-zinc-900 dark:text-white h-screen">
+      <body className="bg-zinc-100 dark:bg-zinc-900 dark:text-white h-screen">
         <Toaster position="top-right" />
         <main>{children}</main>
       </body>
