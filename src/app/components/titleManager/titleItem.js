63a8e84941cde214c0ea6bb98504cd12240ba864
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-05 19:52:57 +0200

    ✨ (titleItem.js): add new component `TitleItem` to manage titles in a list 📝 (titleItem.js): import necessary dependencies and define the `TitleItem` component with props for index, title, event handlers, placeholder text, selection, and single title flag 📝 (titleItem.js): use `useSelector` hook from `react-redux` to access the titles state 📝 (titleItem.js): render the `TitleItem` component with dynamic classes and attributes based on the props and state 📝 (titleItem.js): handle keydown event to prevent default behavior when Enter key is pressed 📝 (titleItem.js): render buttons for selecting and removing titles based on the props and state

---
+++ src/app/components/titleManager/titleItem.js
@@ -0,0 +1,69 @@
+import ContentEditable from "react-contenteditable";
+import { Trash2, CheckCircle, Circle } from "react-feather";
+import { useSelector } from "react-redux";
+
+export default function TitleItem({
+  index,
+  title,
+  handleTitleChange,
+  handleRemoveTitle,
+  placeholderText,
+  onSelect,
+  isSelected,
+  isSingleTitle,
+}) {
+  const titles = useSelector((state) => state.title.titles);
+  return (
+    <div key={index} className={`flex items-center gap-2 `}>
+      <div className="relative w-full">
+        {!title && (
+          <span className="absolute left-2 top-2 text-gray-400 pointer-events-none">
+            {placeholderText}
+          </span>
+        )}
+        <ContentEditable
+          html={title}
+          onChange={(e) => handleTitleChange(index, e)}
+          className={`w-full p-2 rounded-md bg-zinc-200 dark:bg-zinc-700 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:bg-blue-100 active:outline-none  dark:text-white ${
+            title ? "text-black" : "text-gray-400"
+          }`}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              e.preventDefault();
+            }
+          }}
+        />
+        <div className="absolute flex z-20 right-1 top-1 rounded-md bg-white dark:bg-zinc-800">
+          {!isSingleTitle && (
+            <button
+              onClick={(e) => {
+                e.stopPropagation();
+                onSelect(title);
+              }}
+              className="p-2 rounded-md hover:bg-zinc-100 dark:hover:bg-zinc-800 dark:text-white"
+              title={isSelected ? "Selected title" : "Set as selected title"}
+            >
+              {isSelected ? (
+                <CheckCircle className="w-4 h-4 text-green-500" />
+              ) : (
+                <Circle className="w-4 h-4 " />
+              )}
+            </button>
+          )}
+          <div className="h-full w-full bg-zinc-300 dark:bg-zinc-700"></div>
+          {titles.length > 1 && (
+            <button
+              onClick={(e) => {
+                e.stopPropagation();
+                handleRemoveTitle(index);
+              }}
+              className="p-2 rounded-md hover:bg-zinc-100 dark:hover:bg-zinc-600"
+            >
+              <Trash2 className="w-4 h-4" />
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
