8204a284b19cfdaa754d380a5f7a6dc536ab8b37
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-05 22:02:13 +0200

    📝 (page.js): add import statement for Videos component to use it in the Home component ♻️ (page.js): update the structure of the div element to use flexbox layout 🌐 (page.js): add initial props for the Videos component to set the initial region code, category, and language

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
+"use client";
+
 import SideBar from "./components/sideBar/sideBar";
+import Videos from "./components/videos/videos";
 export default function Home() {
   return (
-    <div className="relative p-4">
+    <div className="relative p-4 flex">
       <SideBar />
+      <Videos initialRegionCode="DE" initialCategory="" initialLanguage="de" />
     </div>
   );
 }
