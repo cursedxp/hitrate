e21234a490c8b18590bbbb3b72bd8b9962b8f0a2
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-06 20:12:27 +0200

    ✨ (searchPreview.js): introduce a new component SearchPreview to display a list of preview items 📝 (searchPreview.js): add import statement for PreviewItem component and useSelector hook from react-redux 📝 (searchPreview.js): use useSelector hook to access the previews state from the app slice of the Redux store ✨ (searchPreview.js): render a div container with flex layout and column direction ✨ (searchPreview.js): map over the previews array and render a PreviewItem component for each video in the array

---
+++ src/app/components/previews/searchPreview/searchPreview.js
@@ -0,0 +1,13 @@
+import PreviewItem from "./previewItem";
+import { useSelector } from "react-redux";
+
+export default function SearchPreview() {
+  const previews = useSelector((state) => state.app.previews);
+  return (
+    <div className="flex flex-col gap-4 w-full">
+      {previews.map((video) => (
+        <PreviewItem key={video.id} video={video} />
+      ))}
+    </div>
+  );
+}
