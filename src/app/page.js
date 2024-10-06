f0d2455caabac78be19ac99f436aeae5a742ab07
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-06 17:33:17 +0200

    ✨ (page.js): Replace Videos component with HomePreview component to improve the home page layout and functionality 📝 (page.js): Add PreviewBar component to display a preview bar at the top of the page 📝 (page.js): Add max-width styling to the main content container to improve responsiveness

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,24 @@
 "use client";
 
 import SideBar from "./components/sideBar/sideBar";
-import Videos from "./components/videos/videos";
+import HomePreview from "./components/previews/homePreview/homePreview";
+import PreviewBar from "./components/previewBar/previewBar";
 export default function Home() {
   return (
-    <div className="relative p-4 flex">
+    <div className="relative p-4 flex w-full">
       <SideBar />
-      <Videos initialRegionCode="DE" initialCategory="" initialLanguage="de" />
+      <div className="flex w-full flex-col gap-4 pb-4 px-4">
+        <div className="flex justify-end w-full ">
+          <PreviewBar />
+        </div>
+        <div className="self-center max-w-screen-xl">
+          <HomePreview
+            initialRegionCode="DE"
+            initialCategory=""
+            initialLanguage="de"
+          />
+        </div>
+      </div>
     </div>
   );
 }
