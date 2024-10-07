b23404134cef4f558eaaef6616043cdc06d4ae6c
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-07 22:43:39 +0200

    ✨ (authButton.js): add redux functionality to update user state based on session data 📝 (authButton.js): add useEffect hook to dispatch actions to update user state when session data changes

--- src/app/components/authButton/authButton.js
+++ src/app/components/authButton/authButton.js
@@ -1,9 +1,21 @@
 "use client";
 
 import { useSession, signIn, signOut } from "next-auth/react";
+import { useDispatch } from "react-redux";
+import { setUser, removeUser } from "@/app/redux/slices/user.slice";
+import { useEffect } from "react";
 
 export default function AuthButton() {
   const { data: session } = useSession();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (session) {
+      dispatch(setUser(session.user));
+    } else {
+      dispatch(removeUser());
+    }
+  }, [session, dispatch]);
 
   if (session) {
     return (
