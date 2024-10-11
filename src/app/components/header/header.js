1dbd21142eec13d11ae5c6b3d4457fec52354b5b
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-11 10:51:24 +0200

    ✨ (header.js): add motion animation library import to enable animations in the header component 📝 (header.js): update header component to include a navigation menu with animated list items and an authentication button

--- src/app/components/header/header.js
+++ src/app/components/header/header.js
@@ -1,3 +1,34 @@
+import AuthButton from "../authButton/authButton";
+import { motion } from "framer-motion";
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header className="bg-white">
+      <div className="flex items-center justify-center h-20 ">
+        <nav className="flex items-center justify-between h-full w-full max-w-7xl">
+          <div className="text-2xl font-bold uppercase tracking-widest">
+            HitMagnet
+          </div>
+          <ul className="flex items-center space-x-8">
+            {["Home", "Features", "Pricing", "Contact"].map((item, index) => (
+              <motion.li
+                key={item}
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: index * 0.1 }}
+              >
+                <motion.a
+                  href="#"
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.95 }}
+                >
+                  {item}
+                </motion.a>
+              </motion.li>
+            ))}
+          </ul>
+          <AuthButton />
+        </nav>
+      </div>
+    </header>
+  );
 }
