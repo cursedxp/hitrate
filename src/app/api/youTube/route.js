5391f8b2ee48f2ffe8cf9e69230af9389c073a88
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-12 13:05:14 +0200

    ✨ (route.js): add support for intelligent search functionality in the YouTube API route

--- src/app/api/youTube/route.js
+++ src/app/api/youTube/route.js
@@ -47,6 +47,47 @@ export async function GET(request) {
         });
         return NextResponse.json(trendingResponse.data.items);
 
+      case "intelligentSearch":
+        const query = searchParams.get("query");
+        let channelId;
+
+        // Check if the query looks like a channel ID or username
+        if (/^UC[\w-]{21}[AQgw]$/.test(query)) {
+          channelId = query;
+        } else if (/^@[\w-]+$/.test(query)) {
+          // If it's a username, first get the channel ID
+          const channelResponse = await youtube.channels.list({
+            part: "id",
+            forUsername: query.slice(1), // Remove the '@' symbol
+          });
+          if (
+            channelResponse.data.items &&
+            channelResponse.data.items.length > 0
+          ) {
+            channelId = channelResponse.data.items[0].id;
+          }
+        }
+
+        if (channelId) {
+          // If we have a channel ID, fetch videos for that channel
+          const channelVideosResponse = await youtube.search.list({
+            ...defaultParams,
+            channelId: channelId,
+            order: "date",
+            type: "video",
+          });
+          return NextResponse.json(channelVideosResponse.data);
+        } else {
+          // If it's not a channel search or channel not found, perform a general search
+          const searchResponse = await youtube.search.list({
+            ...defaultParams,
+            q: query,
+            order: "relevance",
+            type: "video",
+          });
+          return NextResponse.json(searchResponse.data);
+        }
+
       default:
         return NextResponse.json(
           { error: "Invalid YouTube endpoint" },
