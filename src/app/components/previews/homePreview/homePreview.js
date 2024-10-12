08d6183334e5f08afaec3b5dcf76ab94be3e305f
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-12 20:23:18 +0200

    ✨ (homePreview.js): add useEffect hook to log updated previews state for debugging purposes ♻️ (homePreview.js): refactor rendering logic to display a message when no previews are available instead of an empty grid

--- src/app/components/previews/homePreview/homePreview.js
+++ src/app/components/previews/homePreview/homePreview.js
@@ -1,15 +1,28 @@
 import PreviewItem from "./previewItem";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 export default function HomePreview() {
+  const searchList = useSelector((state) => state.app.searchList);
   const previews = useSelector((state) => state.app.previews);
+  // const trendingSearch = searchList.find((item) => item.query === "trending");
+
+  useEffect(() => {
+    console.log("Previews state updated:");
+    console.log(JSON.stringify(previews, null, 2));
+  }, [previews]);
+
   return (
     <div className="flex w-full items-center justify-center ">
-      <div className="grid grid-cols-1 lg:grid-cols-4 gap-x-4 gap-y-8 w-full">
-        {previews.map((video) => (
-          <PreviewItem key={video.id} video={video} />
-        ))}
-      </div>
+      {previews.length === 0 ? (
+        <div>No previews available</div>
+      ) : (
+        <div className="grid grid-cols-1 lg:grid-cols-4 gap-x-4 gap-y-8 w-full">
+          {previews.map((video, index) => (
+            <PreviewItem key={index} video={video} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
