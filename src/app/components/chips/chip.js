c7c0b7c1db2d8946e134365ffd4343a64c45d70e
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-12 12:45:25 +0200

    📝 (chip.js): replace removeComparisonList with removeSearchList in handleRemove function to fix incorrect redux action dispatch ♻️ (chips.js): replace setComparisonList and setPreviews with setSearchList in handleInputKeyDown function to improve code semantics and consistency 📝 (chips.js): replace youtubeServices.getSearchResults with fetch API to fetch search results from the server in handleSearch function for better error handling and code organization ♻️ (chips.js): add handleRemoveChip function to handle removing chips from the search list in handleRemoveChip function for better code organization and reusability

--- src/app/components/chips/chip.js
+++ src/app/components/chips/chip.js
@@ -1,17 +1,16 @@
 import { useDispatch } from "react-redux";
-import { removeComparisonList } from "@/app/redux/slices/app.slice";
 import { XMarkIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
+import { removeSearchList } from "@/app/redux/slices/app.slice";
 export default function Chip({ item }) {
   const dispatch = useDispatch();
 
   const handleRemove = () => {
-    dispatch(removeComparisonList(item));
+    dispatch(removeSearchList(item));
   };
 
   return (
     <div className="flex items-center justify-between gap-2 bg-white shadow-sm text-gray-800 px-3 rounded-xl dark:text-white dark:bg-zinc-800 cursor-pointer">
-      <div className="">{item === "trending" ? "🔥 Trending" : item}</div>
+      {item}
       {item !== "trending" && (
         <div className="flex items-center justify-center hover:bg-gray-200 p-1 hover:text-gray-800 rounded-md">
           <XMarkIcon className="w-4 h-4 m-1" onClick={handleRemove} />
--- src/app/components/chips/chips.js
+++ src/app/components/chips/chips.js
@@ -1,14 +1,12 @@
 import { useState } from "react";
-import { PlusIcon } from "@heroicons/react/24/outline";
+import { PlusIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { useDispatch, useSelector } from "react-redux";
-import { setComparisonList, setPreviews } from "@/app/redux/slices/app.slice";
+import { setSearchList, removeSearchList } from "@/app/redux/slices/app.slice";
 import Chip from "./chip";
-import { XMarkIcon } from "@heroicons/react/24/outline";
-import youtubeServices from "@/app/services/youtubeServices";
 
 export default function Chips() {
   const dispatch = useDispatch();
-  const comparisonList = useSelector((state) => state.app.comparisonList);
+  const searchList = useSelector((state) => state.app.searchList);
   const [value, setValue] = useState("");
   const [isAdding, setIsAdding] = useState(false);
 
@@ -23,8 +21,7 @@ export default function Chips() {
   const handleInputKeyDown = async (e) => {
     if (e.key === "Enter" && value.trim()) {
       const data = await handleSearch(value.trim());
-      dispatch(setPreviews(data.items));
-      dispatch(setComparisonList(value.trim()));
+      dispatch(setSearchList({ query: value.trim(), results: data }));
       setValue("");
       setIsAdding(false);
     }
@@ -35,16 +32,28 @@ export default function Chips() {
     setIsAdding(false);
   };
 
-  const handleSearch = async (value) => {
-    const data = await youtubeServices.getSearchResults(value);
-    console.log(data.items);
-    return data;
+  const handleRemoveChip = (endpoint) => {
+    dispatch(removeSearchList(endpoint));
+  };
+
+  const handleSearch = async (query) => {
+    try {
+      const response = await fetch(`/api/youTube?endpoint=search&q=${query}`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error fetching search results:", error);
+    }
   };
 
   return (
     <div className="flex gap-2">
-      {comparisonList.map((item, index) => (
-        <Chip key={index} item={item} />
+      {searchList.map((item, index) => (
+        <Chip
+          key={index}
+          item={item.query}
+          onRemove={() => handleRemoveChip(item.query)}
+        />
       ))}
 
       {isAdding ? (
