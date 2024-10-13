7aef7ccd02eb81b88972b2ee98f377a10839d599
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-13 14:33:10 +0200

    ♻️ (sideBar.js): remove unnecessary overflow-scroll class from sideBar component to improve code readability and maintainability

--- src/app/components/sideBar/sideBar.js
+++ src/app/components/sideBar/sideBar.js
@@ -21,9 +21,7 @@ const tabs = [
 
 export default function SideBar() {
   return (
-    <div
-      className={`bg-white shadow-md dark:bg-zinc-800 rounded-xl w-[300px] overflow-scroll`}
-    >
+    <div className={`bg-white shadow-md dark:bg-zinc-800 rounded-xl w-[300px]`}>
       <UserDetails />
       <div className="flex gap-2 px-2 py-2 items-center border-b border-zinc-200 dark:border-zinc-700">
         <Button className="bg-white">
