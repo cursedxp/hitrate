44f047aefc99f388e79b4781d26649a1a78931ed
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-13 15:04:53 +0200

    ♻️ (homePreview.js): refactor HomePreview component to combine previews and search results into a single array for rendering ✨ (homePreview.js): add support for displaying search results in addition to previews in the HomePreview component

--- src/app/components/previews/homePreview/homePreview.js
+++ src/app/components/previews/homePreview/homePreview.js
@@ -1,23 +1,27 @@
 import PreviewItem from "./previewItem";
 import { useSelector } from "react-redux";
-import { useEffect } from "react";
 
 export default function HomePreview() {
   const searchList = useSelector((state) => state.app.searchList);
   const previews = useSelector((state) => state.app.previews);
-  // const trendingSearch = searchList.find((item) => item.query === "trending");
+  const selectedSearchItem = useSelector(
+    (state) => state.app.selectedSearchItem
+  );
+
+  // Combine previews and search results
+  const allPreviews = [
+    ...previews,
+    ...(searchList.find((item) => item.query === selectedSearchItem)?.results ||
+      []),
+  ];
 
   return (
     <div className="flex w-full items-center justify-center ">
-      {previews.length === 0 ? (
-        <div>No previews available</div>
-      ) : (
-        <div className="grid grid-cols-1 lg:grid-cols-4 gap-x-4 gap-y-8 w-full">
-          {previews.map((video, index) => (
-            <PreviewItem key={index} video={video} />
-          ))}
-        </div>
-      )}
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-x-4 gap-y-8 w-full">
+        {allPreviews.map((video, index) => (
+          <PreviewItem key={index} video={video} />
+        ))}
+      </div>
     </div>
   );
 }
