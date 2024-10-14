29031338b2c94af75f761958e4c178e283af7bb9
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-14 11:59:18 +0200

    ♻️ (previewItem.js): refactor the logic for rendering the channel avatar in the PreviewItem component to improve readability and maintainability

--- src/app/components/previews/searchPreview/previewItem.js
+++ src/app/components/previews/searchPreview/previewItem.js
@@ -39,15 +39,28 @@ export default function PreviewItem({ video }) {
             </span>
           </div>
           <div className="flex gap-2 items-center">
-            <div className="relative h-6 w-6">
-              <Image
-                src={channelAvatar || "/default-avatar.png"}
-                alt={video.snippet.channelTitle}
-                sizes="100%"
-                fill
-                className="object-cover rounded-full"
-              />
-            </div>
+            {channelAvatar ? (
+              <div className="relative h-6 w-6">
+                <Image
+                  src={channelAvatar || "/default-avatar.png"}
+                  alt={video.snippet.channelTitle}
+                  sizes="100%"
+                  fill
+                  className="object-cover rounded-full"
+                />
+              </div>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                className="w-6 h-6"
+              >
+                <path
+                  fill="#FF0000"
+                  d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"
+                />
+              </svg>
+            )}
             <span className="text-xs text-gray-500">
               {video.snippet.channelTitle}
             </span>
