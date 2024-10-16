f172ae50f69e8dd359dee8b428c5f79e6f5057b4
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-16 09:37:34 +0200

    🔧 (next.config.mjs): add "picsum.photos" to the list of allowed hostnames in the nextConfig object to allow loading images from that domain

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const nextConfig = {
       { hostname: "yt3.ggpht.com" },
       { hostname: "img.youtube.com" },
       { hostname: "lh3.googleusercontent.com" },
+      { hostname: "picsum.photos" },
     ],
   },
 };
