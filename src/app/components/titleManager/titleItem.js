817d2f750ab4365298606bb6c2b9d668d7cda704
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-16 10:58:14 +0200

    ♻️ (titleItem.js): refactor TitleItem component's className to improve readability and maintainability

--- src/app/components/titleManager/titleItem.js
+++ src/app/components/titleManager/titleItem.js
@@ -31,7 +31,7 @@ export default function TitleItem({
         <ContentEditable
           html={title}
           onChange={(e) => handleLocalTitleChange(index, e)}
-          className={`w-full p-2 rounded-md bg-zinc-200 dark:bg-zinc-700 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:bg-blue-100 active:outline-none  dark:text-white ${
+          className={`w-full p-2 rounded-lg border dark:bg-zinc-700 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:bg-blue-100 active:outline-none  dark:text-white ${
             title ? "text-black" : "text-gray-400"
           }`}
           onKeyDown={(e) => {
