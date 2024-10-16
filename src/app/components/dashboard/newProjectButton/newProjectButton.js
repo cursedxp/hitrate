8397785170ed7e9e8e33b79064aeb582e1e6f6df
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-16 10:57:19 +0200

    ✨ (newProjectButton.js): add onClick prop to NewProjectButton component to handle button click event

--- src/app/components/dashboard/newProjectButton/newProjectButton.js
+++ src/app/components/dashboard/newProjectButton/newProjectButton.js
@@ -1,8 +1,11 @@
 import { Plus } from "react-feather";
-export default function NewProjectButton() {
+export default function NewProjectButton({ onClick }) {
   return (
     <div>
-      <button className="relative h-48 rounded-xl w-full border-2 border-blue-500 flex flex-col justify-center items-center hover:scale-105 transition-all duration-300 hover:shadow-lg">
+      <button
+        className="relative h-48 rounded-xl w-full border-2 border-blue-500 flex flex-col justify-center items-center hover:scale-105 transition-all duration-300 hover:shadow-lg"
+        onClick={onClick}
+      >
         <div className="rounded-full text-blue-500 bg-blue-200 w-12 h-12 flex justify-center items-center">
           <Plus className="w-6 h-6" />
         </div>
