f8ff41f928e1fc8131a493aa443f4e83630a2135
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-17 20:03:54 +0200

    🐛 (route.js): remove unnecessary import of NextRequest from next/server ♻️ (route.js): refactor POST function to use destructuring assignment to get priceId from request body and improve code readability ✨ (route.js): update success_url and cancel_url to use req.headers.get() instead of req.headers.origin to support different origin headers

--- src/app/api/stripe/route.js
+++ src/app/api/stripe/route.js
@@ -1,24 +1,23 @@
 import Stripe from "stripe";
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-export async function POST(req, res) {
-  if (req.method !== "POST") {
-    try {
-      const session = await stripe.checkout.sessions.create({
-        mode: "subscription",
-        payment_method_types: ["card", "paypal"],
-        line_items: [{ price: price_1QAq2SPryQLXqSIpN2rPAPy8, quantity: 1 }],
-        success_url: `${req.headers.origin}/success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${req.headers.origin}/cancel`,
-      });
-      res.status(200).json({ sessionId: session.id });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
 
-    return NextResponse.json({ error: "Method not allowed" }, { status: 405 });
-  } else {
-    return NextResponse.json({ error: "Method not allowed" }, { status: 405 });
+export async function POST(req) {
+  try {
+    const { priceId } = await req.json(); // Get the selected price ID from the request body
+
+    const session = await stripe.checkout.sessions.create({
+      mode: "subscription",
+      payment_method_types: ["card"],
+      line_items: [{ price: priceId, quantity: 1 }],
+      success_url: `${req.headers.get(
+        "origin"
+      )}/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${req.headers.get("origin")}/cancel`,
+    });
+    return NextResponse.json({ sessionId: session.id });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
