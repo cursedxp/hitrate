16db5e77da4206cb2ca2caf38b080f87334b6cc1
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-18 10:14:36 +0200

    ✨ (page.js): improve layout and styling of the Home page 📝 (page.js): update text content to better describe the purpose of the app 🚀 (page.js): add a button to launch the app

--- src/app/page.js
+++ src/app/page.js
@@ -5,40 +5,52 @@ export default function Home() {
   return (
     <>
       <Header />
-      <div className="flex flex-col items-center justify-center bg-white w-full h-screen">
-        <div className="flex flex-col h-full w-full max-w-7xl bg-white">
-          <div className="flex flex-col justify-center items-center  text-center text-zinc-900 min-h-min p-40">
-            <h1 className="text-9xl font-bold mb-4">
-              <span className="">Craft stunning </span>
-              <motion.span
-                className="bg-clip-text text-transparent bg-gradient-to-r from-red-500 via-yellow-500 via-green-500 via-blue-500 to-purple-500"
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.8, ease: "easeOut" }}
-              >
-                {["t", "h", "u", "m", "b", "n", "a", "i", "l", "s"].map(
-                  (letter, index) => (
-                    <motion.span
-                      key={index}
-                      initial={{ opacity: 0, y: 20 }}
-                      animate={{ opacity: 1, y: 0 }}
-                      transition={{ duration: 0.5, delay: index * 0.1 }}
-                    >
-                      {letter}
-                    </motion.span>
-                  )
-                )}
-              </motion.span>
-            </h1>
-            <p className="text-2xl px-20 text-zinc-500 mb-8 leading-relaxed">
-              Elevate your content's appeal with HitMagnet. Our tool analyzes
-              trends to help you create eye-catching visuals that drive clicks
-              and views.
-            </p>
-            <div className="flex items-center justify-center">
-              <button className="bg-blue-500 text-xl  text-white px-8 py-4 rounded-2xl  hover:shadow-2xl transition-shadow duration-300 hover:scale-105 ">
-                Launch App
-              </button>
+      <div className="flex flex-col items-center bg-white w-full h-screen">
+        <div className="flex flex-col items-center justify-center">
+          <div
+            className="absolute h-[1000px] inset-0 opacity-50"
+            style={{
+              backgroundImage: `
+            linear-gradient(to right, #c0c0c0 1px, transparent 1px),
+            linear-gradient(to bottom, #c0c0c0 1px, transparent 1px)
+          `,
+              backgroundSize: "100px 100px",
+            }}
+          ></div>
+          <div className="flex flex-col h-full w-full max-w-7xl z-10">
+            <div className="flex flex-col justify-center items-center  text-center text-zinc-900 min-h-min p-40">
+              <h1 className="text-9xl font-bold mb-4">
+                <span className="">Craft stunning </span>
+                <motion.span
+                  className="bg-clip-text text-transparent bg-gradient-to-r from-red-500 via-yellow-500 via-green-500 via-blue-500 to-purple-500"
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.8, ease: "easeOut" }}
+                >
+                  {["t", "h", "u", "m", "b", "n", "a", "i", "l", "s"].map(
+                    (letter, index) => (
+                      <motion.span
+                        key={index}
+                        initial={{ opacity: 0, y: 20 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.5, delay: index * 0.1 }}
+                      >
+                        {letter}
+                      </motion.span>
+                    )
+                  )}
+                </motion.span>
+              </h1>
+              <p className="text-2xl px-20 text-zinc-500 mb-8 leading-relaxed">
+                Elevate your content's appeal with HitMagnet. Our tool analyzes
+                trends to help you create eye-catching visuals that drive clicks
+                and views.
+              </p>
+              <div className="flex items-center justify-center">
+                <button className="bg-blue-500 text-xl  text-white px-8 py-4 rounded-2xl  hover:shadow-2xl transition-shadow duration-300 hover:scale-105 ">
+                  Launch App
+                </button>
+              </div>
             </div>
           </div>
         </div>
