8acc0a6e4ad8acd6ac1cbe66187c0f98138b7833
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-19 13:48:08 +0200

    ✨ (route.js): add authentication check to ensure user is logged in before creating a checkout session 🐛 (route.js): fix incorrect variable name in checkout session creation, change session to checkoutSession 📝 (route.js): add client_reference_id and customer_email to checkout session to associate the session with the user and provide their email for communication purposes 🐛 (route.js): fix error handling in case of failure to create checkout session, log error message and return appropriate response

--- src/app/api/stripe/route.js
+++ src/app/api/stripe/route.js
@@ -1,13 +1,27 @@
 import Stripe from "stripe";
 import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]/route";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(req) {
-  try {
-    const { priceId } = await req.json();
+  const session = await getServerSession(authOptions);
 
-    const session = await stripe.checkout.sessions.create({
+  if (!session || !session.user) {
+    return new NextResponse(
+      JSON.stringify({
+        error: "You must be logged in to create a checkout session",
+      }),
+      { status: 401 }
+    );
+  }
+
+  const { priceId } = await req.json();
+  const userId = session.user.id; // This should be the Google user ID
+
+  try {
+    const checkoutSession = await stripe.checkout.sessions.create({
       mode: "subscription",
       payment_method_types: ["card", "paypal"],
       line_items: [{ price: priceId, quantity: 1 }],
@@ -15,10 +29,19 @@ export async function POST(req) {
         "origin"
       )}/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${req.headers.get("origin")}/pricing`,
+      client_reference_id: userId,
+      customer_email: session.user.email,
     });
-    return NextResponse.json({ sessionId: session.id });
+
+    return new NextResponse(JSON.stringify({ sessionId: checkoutSession.id }), {
+      status: 200,
+    });
   } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    console.error("Error creating checkout session:", error);
+    return new NextResponse(
+      JSON.stringify({ error: "Error creating checkout session" }),
+      { status: 500 }
+    );
   }
 }
 
