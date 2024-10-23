214cac074bda313e9a3752b67906082cd9923be5
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-23 12:28:07 +0200

    ♻️ (titleItem.js): refactor TitleItem component to improve readability and remove unnecessary code ✨ (titleItem.js): add support for displaying a single title differently by checking the length of the titles array

--- src/app/components/titleManager/titleItem.js
+++ src/app/components/titleManager/titleItem.js
@@ -10,9 +10,9 @@ export default function TitleItem({
   placeholderText,
   onSelect,
   isSelected,
-  isSingleTitle,
 }) {
   const titles = useSelector((state) => state.title.titles);
+  const isSingleTitle = titles.length === 1;
 
   // Add this new function to handle the &nbsp; issue
   const handleLocalTitleChange = (index, e) => {
@@ -21,56 +21,54 @@ export default function TitleItem({
   };
 
   return (
-    <div key={index} className={`flex items-center gap-2 `}>
-      <div className="relative w-full">
-        {!title && (
-          <span className="absolute left-2 top-2 text-gray-400 pointer-events-none">
-            {placeholderText}
-          </span>
-        )}
-        <ContentEditable
-          html={title}
-          onChange={(e) => handleLocalTitleChange(index, e)}
-          className={`w-full p-2 rounded-lg border dark:bg-zinc-700 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:bg-blue-100 active:outline-none  dark:text-white ${
-            title ? "text-black" : "text-gray-400"
-          }`}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              e.preventDefault();
-            }
-          }}
-        />
-        <div className="absolute flex z-20 right-1 top-1 rounded-md bg-white dark:bg-zinc-800">
-          {!isSingleTitle && (
-            <button
-              onClick={(e) => {
-                e.stopPropagation();
-                onSelect(title);
-              }}
-              className="p-2 rounded-md hover:bg-zinc-100 dark:hover:bg-zinc-800 dark:text-white"
-              title={isSelected ? "Selected title" : "Set as selected title"}
-            >
-              {isSelected ? (
-                <CheckCircle className="w-4 h-4 text-green-500" />
-              ) : (
-                <Circle className="w-4 h-4 " />
-              )}
-            </button>
-          )}
-          <div className="h-full w-full bg-zinc-300 dark:bg-zinc-700"></div>
-          {titles.length > 1 && (
-            <button
-              onClick={(e) => {
-                e.stopPropagation();
-                handleRemoveTitle(index);
-              }}
-              className="p-2 rounded-md hover:bg-zinc-100 dark:hover:bg-zinc-600"
-            >
-              <Trash2 className="w-4 h-4" />
-            </button>
-          )}
+    <div key={index} className="flex w-full">
+      {!title && (
+        <span className="absolute left-2 top-2 text-gray-400 pointer-events-none">
+          {placeholderText}
+        </span>
+      )}
+      <ContentEditable
+        html={title}
+        onChange={(e) => handleLocalTitleChange(index, e)}
+        className={`w-full p-2 rounded-lg border dark:bg-zinc-700 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:bg-blue-100 active:outline-none  dark:text-white ${
+          title ? "text-black" : "text-gray-400"
+        }`}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+          }
+        }}
+      />
+      {!isSingleTitle && (
+        <div className="flex h-16 flex-col rounded-md ml-2 dark:bg-zinc-800">
+          <button
+            onClick={(e) => {
+              e.stopPropagation();
+              onSelect(title);
+            }}
+            className="p-2 rounded-md hover:bg-zinc-100 dark:hover:bg-zinc-800 dark:text-white"
+            title={isSelected ? "Selected title" : "Set as selected title"}
+          >
+            {isSelected ? (
+              <CheckCircle className="w-4 h-4 text-green-500" />
+            ) : (
+              <Circle className="w-4 h-4 " />
+            )}
+          </button>
+
+          <div className="h-full bg-zinc-300 dark:bg-zinc-700"></div>
+
+          <button
+            onClick={(e) => {
+              e.stopPropagation();
+              handleRemoveTitle(index);
+            }}
+            className="p-2 rounded-md hover:bg-zinc-100 dark:hover:bg-zinc-600"
+          >
+            <Trash2 className="w-4 h-4" />
+          </button>
         </div>
-      </div>
+      )}
     </div>
   );
 }
