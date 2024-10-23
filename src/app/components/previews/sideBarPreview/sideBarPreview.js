50b32e2572418ce01b71d29681290a29ec143326
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-23 15:15:04 +0200

    ♻️ (sideBarPreview.js): refactor SideBarPreview component to center align its child elements

--- src/app/components/previews/sideBarPreview/sideBarPreview.js
+++ src/app/components/previews/sideBarPreview/sideBarPreview.js
@@ -5,7 +5,7 @@ export default function SideBarPreview() {
   const allPreviews = useSelector((state) => state.app.allPreviews);
 
   return (
-    <div className="flex flex-col gap-4 w-full">
+    <div className="flex flex-col items-center gap-4 w-full">
       {allPreviews.map((video) => (
         <PreviewItem key={video.id} video={video} />
       ))}
