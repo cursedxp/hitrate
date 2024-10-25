c5f31476d581ceba83c6113663d484b76ad5dc52
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-25 14:45:38 +0200

    ✨ (page.js): add ChevronDown icon from react-feather library to improve UI ♻️ (page.js): remove unused imports and components to clean up code ✨ (page.js): add trial badge and upgrade button to enhance user experience ♻️ (page.js): refactor user information section to improve readability and add dropdown functionality ♻️ (page.js): refactor navigation menu to improve readability and add active state functionality

--- src/app/studio/page.js
+++ src/app/studio/page.js
@@ -2,15 +2,15 @@
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
-import { Plus } from "react-feather";
+import { Plus, ChevronDown } from "react-feather";
 import ProjectItem from "../components/dashboard/projectItem/projectItem";
 import NewProjectButton from "../components/dashboard/newProjectButton/newProjectButton";
-import Modal from "../components/modal/modal";
+
 import { clearProjectData } from "@/app/redux/slices/app.slice";
 import { clearThumbnailData } from "@/app/redux/slices/thumbnail.slice";
 import { clearTitleData } from "@/app/redux/slices/title.slice";
 import { useDispatch } from "react-redux";
-import Loader from "@/app/components/loader/loader";
+
 export default function Dashboard() {
   const dispatch = useDispatch();
   const [isActive, setIsActive] = useState("Projects");
@@ -53,7 +53,7 @@ export default function Dashboard() {
 
   return (
     <div className="flex flex-col max-w-7xl py-32 h-screen mx-auto">
-      <div className="flex flex-col relative gap-4 text-center items-center space-x-4">
+      <div className="flex relative gap-4 items-center justify-center space-x-4 ">
         <div className="w-22 rounded-full p-1 bg-gradient-to-br from-pink-500 via-blue-500 animate-gradient-x">
           <Image
             src={session?.user?.image}
@@ -63,28 +63,39 @@ export default function Dashboard() {
             className="rounded-full"
           />
         </div>
-        <div>
-          <h1 className="text-2xl font-bold">{session?.user?.name}</h1>
-          <p className="text-gray-600">{session?.user?.email}</p>
+        <div className="flex flex-col gap-1">
+          <div className="flex gap-2 items-center">
+            <div className="text-xl font-semibold">{session?.user?.name}</div>
+            <div className="flex h-6 items-center justify-center text-xs text-center bg-blue-100 text-blue-500  font-medium rounded-md px-2">
+              Trial
+            </div>
+            <div className="flex w-6 h-6 items-center justify-center hover:cursor-pointer hover:bg-zinc-100 rounded-md">
+              <ChevronDown className="w-4 h-4" />
+            </div>
+          </div>
+          <div className="text-gray-600">{session?.user?.email}</div>
+          <div className="flex items-center text-sm gap-2 w-36 bg-yellow-200 text-yellow-800 px-2 py-1 rounded-md cursor-pointer">
+            <span className="text-sm">👑</span> Upgrade to Pro
+          </div>
         </div>
-        <nav className="">
-          <ul className="flex mt-4 space-x-4">
-            {["Projects"].map((item) => (
-              <li key={item}>
-                <a
-                  href={`#${item.toLowerCase().replace(" ", "-")}`}
-                  className={`bg-zinc-100 px-4 py-2 rounded-lg text-gray-500 hover:text-gray-900 hover:bg-zinc-200 ${
-                    isActive === item ? "bg-zinc-200" : ""
-                  }`}
-                  onClick={() => setIsActive(item)}
-                >
-                  {item}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </nav>
       </div>
+      <nav className="flex justify-center mt-8">
+        <ul className="flex mt-4 space-x-4">
+          {["Projects"].map((item) => (
+            <li key={item}>
+              <a
+                href={`#${item.toLowerCase().replace(" ", "-")}`}
+                className={`bg-zinc-100 px-4 py-2 rounded-lg text-gray-500 hover:text-gray-900 hover:bg-zinc-200 ${
+                  isActive === item ? "bg-zinc-200" : ""
+                }`}
+                onClick={() => setIsActive(item)}
+              >
+                {item}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
       {projects.length > 11 && (
         <div className="absolute right-4 top-4 flex items-center space-x-4">
           <button className="bg-blue-500 text-white p-4 rounded-full hover:bg-blue-600">
