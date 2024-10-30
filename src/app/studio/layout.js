af50af364455a6cef6711eecc07f61cfeccd871d
Anil Ozsoy <anilozsoy@gmail.com>
2024-10-30 12:36:59 +0100

    📝 (layout.js): remove unused import of Loader component to improve code cleanliness and reduce bundle size ✨ (layout.js): add redirect to home page ("/") when user is unauthenticated to improve user experience ♻️ (layout.js): refactor useEffect dependencies to include router to prevent unnecessary re-renders

--- src/app/studio/layout.js
+++ src/app/studio/layout.js
@@ -4,7 +4,7 @@ import { useRouter } from "next/navigation";
 import { useDispatch } from "react-redux";
 import { setUser } from "@/app/redux/slices/user.slice";
 import { useEffect } from "react";
-import Loader from "@/app/components/loader/loader";
+
 export default function StudioLayout({ children }) {
   const { data: session, status } = useSession();
   const router = useRouter();
@@ -13,17 +13,18 @@ export default function StudioLayout({ children }) {
   useEffect(() => {
     if (status === "authenticated" && session) {
       dispatch(setUser(session.user));
+    } else if (status === "unauthenticated") {
+      router.push("/");
     }
-  }, [status, session, dispatch]);
+  }, [status, session, dispatch, router]);
 
   if (status === "loading") {
-    return <Loader />;
+    return null;
   }
 
   if (status === "authenticated") {
     return <div>{children}</div>;
   }
 
-  router.push("/");
   return null;
 }
