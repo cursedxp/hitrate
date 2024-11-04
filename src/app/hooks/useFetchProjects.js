79ad4db51fcd895cc74f37e97e8aaef846d33f8c
Anil Ozsoy <anilozsoy@gmail.com>
2024-11-04 15:08:20 +0100

    ♻️ (useFetchProjects.js): refactor useFetchProjects hook to improve code readability and performance ✨ (useFetchProjects.js): add refresh function to manually trigger fetching projects

--- src/app/hooks/useFetchProjects.js
+++ src/app/hooks/useFetchProjects.js
@@ -1,31 +1,30 @@
-import { useState, useEffect } from "react";
+import { useState, useCallback, useEffect } from "react";
 
 export function useFetchProjects() {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [projects, setProjects] = useState([]);
 
-  useEffect(() => {
-    const fetchProjects = async () => {
-      setLoading(true);
-      try {
-        const response = await fetch("/api/projects/read");
-        if (!response.ok) {
-          throw new Error("Failed to fetch projects");
-        }
-        const data = await response.json();
-        const sortedProjects = data.projects.sort((a, b) => {
-          return new Date(b.createdAt) - new Date(a.createdAt);
-        });
-        setProjects(sortedProjects);
-      } catch (error) {
-        console.error("Error fetching projects:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchProjects();
+  const fetchProjects = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await fetch("/api/projects/read");
+      if (!response.ok) throw new Error("Failed to fetch projects");
+      const data = await response.json();
+      setProjects(data.projects);
+    } catch (error) {
+      console.error("Error fetching projects:", error);
+    } finally {
+      setLoading(false);
+    }
   }, []);
 
-  return { loading, projects };
+  const refresh = useCallback(() => {
+    fetchProjects();
+  }, [fetchProjects]);
+
+  useEffect(() => {
+    fetchProjects();
+  }, [fetchProjects]);
+
+  return { loading, projects, refresh };
 }
