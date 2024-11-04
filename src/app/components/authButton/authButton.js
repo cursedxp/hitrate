9972675d97271f14f20f635e683fbd2b7533e3bf
Anil Ozsoy <anilozsoy@gmail.com>
2024-11-04 09:30:39 +0100

    ✨ (authButton.js): add callbackUrl parameter to signOut function to redirect user to the home page after signing out ✨ (authButton.js): add callbackUrl parameter to signIn function to redirect user to the studio page after signing in

--- src/app/components/authButton/authButton.js
+++ src/app/components/authButton/authButton.js
@@ -20,7 +20,7 @@ export default function AuthButton() {
         <div className="flex flex-col text-sm">
           <span>{session.user.name}</span>
           <div
-            onClick={() => signOut()}
+            onClick={() => signOut({ callbackUrl: "/" })}
             className="flex items-center gap-2 text-zinc-400 rounded-md text-xs cursor-pointer underline"
           >
             Sign out
@@ -32,8 +32,8 @@ export default function AuthButton() {
   return (
     <div className="flex flex-col items-center">
       <button
-        onClick={() => signIn("google")}
-        className="px-4 py-2 border-2 text-blue-500 border-blue-500   rounded-xl hover:bg-blue-600 hover:text-white  transition-colors flex items-center"
+        onClick={() => signIn("google", { callbackUrl: "/studio" })}
+        className="px-4 py-2 border-2 text-blue-500 border-blue-500 rounded-xl hover:bg-blue-600 hover:text-white transition-colors flex items-center"
       >
         Sign in
       </button>
