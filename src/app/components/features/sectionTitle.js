93e814859dee2c87fe4e84e62ae7c3fe87f82aca
Anil Ozsoy <anilozsoy@gmail.com>
2025-01-02 12:02:32 +0100

    ✨ (sectionTitle.js): increase font size of title and description for better readability and visual appeal ✨ (page.js): add margin top to the section to improve spacing and layout ✨ (page.js): increase font size of hero section title for better visual impact and emphasis

--- src/app/components/features/sectionTitle.js
+++ src/app/components/features/sectionTitle.js
@@ -1,8 +1,12 @@
 export default function SectionTitle({ title, description, paragraphStyle }) {
   return (
     <div className="text-center w-full flex flex-col items-center mb-16">
-      <h1 className="text-6xl font-bold text-center mb-6 max-w-3xl">{title}</h1>
-      <p className={`text-xl text-gray-500 text-center ${paragraphStyle}`}>
+      <h1 className="lg:text-7xl md:text-5xl sm:text-5xl font-bold text-center mb-6 max-w-3xl">
+        {title}
+      </h1>
+      <p
+        className={`text-xl md:text-lg text-gray-500 text-center ${paragraphStyle}`}
+      >
         {description}
       </p>
     </div>
